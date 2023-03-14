3db1a80e9bacf5bc17a5a205b966ac0b5ed90244
Marcelo Lemos <108501838+Lemmores@users.noreply.github.com>
2023-03-14 09:29:55 -0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Nosso Footer!</p>
+      <p>© Copyright - Marcelo Lemos - 2023.</p>
     </footer>
   )
 }
